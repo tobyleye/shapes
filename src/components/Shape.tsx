a096f294a3d530c1c0e707683c80f4198df59a41
tobi <krisella74@gmail.com>
2021-07-07 14:42:42 +0100

    dadd shapes types

--- src/components/Shape.tsx
+++ src/components/Shape.tsx
@@ -1,17 +1,29 @@
 import { Rectangle, Circle, Ellipse, Triangle } from "react-shapes";
 
+type ShapeProps = { color: string }
+type OvalProps = ShapeProps & {
+  radius?: number;
+}
+type RoundProps = ShapeProps & {
+  radius?: number;
+}
+type SquareProps = ShapeProps & { length?: number}
+type RectangleProps = ShapeProps & { width?: number; height?: number}
+type TriangleProps = ShapeProps & { width?:number; height?: number;}
+
+
 const shapes = {
-  oval: ({ color, radius = 50 }) => (
-    <Ellipse rx={40} ry={60} fill={{ color }} />
+  oval: ({ color, radius = 50 }: OvalProps) => (
+    <Ellipse rx={radius} ry={radius} fill={{ color }} />
   ),
-  round: ({ color, radius = 50 }) => <Circle r={radius} fill={{ color }} />,
-  triangle: ({ color, width = 100, height = 100 }) => (
+  round: ({ color, radius = 50 }: RoundProps) => <Circle r={radius} fill={{ color }} />,
+  triangle: ({ color, width = 100, height = 100 }: TriangleProps) => (
     <Triangle width={width} height={height} fill={{ color }} />
   ),
-  square: ({ color, length = 100 }) => (
+  square: ({ color, length = 100 }: SquareProps) => (
     <Rectangle width={length} height={length} fill={{ color }} />
   ),
-  rectangle: ({ color, width = 120, height = 80 }) => (
+  rectangle: ({ color, width = 120, height = 80 }: RectangleProps) => (
     <Rectangle width={width} height={height} fill={{ color }} />
   )
 };
