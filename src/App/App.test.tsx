37ef1873c4d4be6dfdef36e1dcef66f758e7b8fa
tobi <krisella74@gmail.com>
2021-07-09 23:45:31 +0100

    pass first test 🎉

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import App from '.';
+import { AuthProvider } from "../contexts/Auth"
 
 test('renders without crashing', () => {
-  render(<App />);
+  render(<AuthProvider><App /></AuthProvider>);
 });
